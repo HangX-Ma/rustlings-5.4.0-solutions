16578483f5ceff20e9fad6d4fc60ec884e137e01
MContour <m-contour@qq.com>
2023-03-13 18:27:00 +0800

    finish all

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let ch: Vec<&str> = s.split(',').collect();
+        if ch.len() != 2 {
+            return Person::default();
+        }
+        let name: String = ch[0].to_string(); 
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let age  = ch[1].parse::<usize>();
+        if let Ok(age) = age {
+            Person {name: name, age: age}
+        } else {
+            Person::default()
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,7 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -46,6 +45,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<&str> = s.split(',').collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = v[0].to_string();
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+        let age_res = v[1].parse::<usize>();
+        match age_res {
+            Ok(age) => Ok(Person {name: name, age: age}),
+            Err(err) => Err(ParsePersonError::ParseInt(err)),
+        }
     }
 }
 
